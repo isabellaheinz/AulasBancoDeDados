768e45cddbadf5ef2618ef91662f106085fc2b4b
Isabella Heinz <isabella.heinz12@gmail.com>
2026-07-29 11:16:23 -0300

    Update trabalhoCartas.php

--- 2°Bimestre/Aula8/trabalhoCartas.php
+++ 2°Bimestre/Aula8/trabalhoCartas.php
@@ -8,7 +8,11 @@ class Carta {
     public function __toString()
     {
         $dados = "N° da Carta: " . $this->numero . " | Personagem: " . $this->nome . "\n";
-        $dados .= "Dica: " . $this->dica . ".\n";
+        return $dados;
+    }
+
+    public function exibirDica() {
+        $dados = "Dica: "  . $this->dica . "\n";
         return $dados;
     }
 
@@ -52,7 +56,7 @@ class Carta {
 
 $BaralhoBarbies = array();
 
-/* Criação das Cartas dos Personagens de Livros: */
+/* Criação das Cartas das Personagens de filmes da Barbie: */
 
 $carta = new Carta;
 $carta->setNumero(1);
@@ -93,8 +97,66 @@ array_push($BaralhoBarbies, $carta);
 $carta = new Carta;
 $carta->setNumero(7);
 $carta->setNome("Liana");
-$carta->setDica("Esta personagem tem um colar da amizade com sua amiga.-");
+$carta->setDica("Esta personagem tem um colar da amizade com sua amiga.");
 array_push($BaralhoBarbies, $carta);
 
 
+$CartaSorteada = $BaralhoBarbies[array_rand($BaralhoBarbies)];
 
+echo "Bem vindo ao baralho de cartas da Barbie!\n";
+echo "Cada carta contém uma personagem de algum filme da Barbie.\n";
+echo "Você deve tentar adivinhar a carta que foi sorteada!\n\n";
+
+do {
+
+echo "\nO que você deseja fazer?\n";
+echo "1- Exibir Cartas Disponivéis.\n";
+echo "2- Exibir Dica da carta Sorteada.\n";
+echo "3- Dar um palpite.\n";
+echo "4- Desistir.\n";
+$opcao = readline("Resposta: ");
+
+switch ($opcao) {
+    case 1:
+        echo "\nCartas Disponivéis: \n\n";
+
+        foreach ($BaralhoBarbies as $carta) {
+        echo $carta . "\n";
+        }
+        break;
+
+    case 2: 
+
+        echo "\nDica da carta sorteada:\n";
+        echo $CartaSorteada->exibirDica();
+        break;
+    
+    case 3:
+
+        echo "\nDe seu palpite de qual foi a carta sorteada!\n";
+        echo "Lembre-se de digitar o número da carta como chute.\n";
+        $resposta = readline("Resposta: "); 
+
+        if ($resposta == $CartaSorteada->getNumero()) {
+            echo "\nParabens! Você acertou a carta sorteada.\n";
+            $opcao = 0;
+        } else if ($resposta != $CartaSorteada->getNumero()) {
+            echo "\nInfelizmente você não acertou. Tente novamente.\n";
+        }
+
+        break;
+
+    case 4:
+        echo "\nQue pena que desistiu!\n";
+        echo "A carta sorteada era:\n";
+        echo $CartaSorteada;
+        echo "\nObrigada por jogar!\n";
+        $opcao = 0;
+        break;
+    
+    default:
+        echo "Opcão inválida. Tente Novamente.\n";
+        break;
+}
+
+} while ($opcao != 0);
