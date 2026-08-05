563a4c302e956f4108c51cd649d33817f2268c6f
Isabella Heinz <isabella.heinz12@gmail.com>
2026-08-05 15:46:22 -0300

    Add files via upload

---
+++ 3°Bimestre/Aula2/AssociaçãoCarro/execucao.php
@@ -0,0 +1,82 @@
+<?php
+
+require_once("Modelo/Carro.php");
+require_once("Modelo/Fabricante.php");
+
+$fabricantes = array();
+
+$fabricante = new Fabricante;
+$fabricante->setNome("Volkswagem");
+$fabricante->setSigla("VW");
+array_push($fabricantes,$fabricante);
+
+$fabricante = new Fabricante;
+$fabricante->setNome("Chevrolet");
+$fabricante->setSigla("GM");
+array_push($fabricantes,$fabricante);
+
+$fabricante = new Fabricante;
+$fabricante->setNome("Fiat");
+$fabricante->setSigla("F");
+array_push($fabricantes,$fabricante);
+
+$fabricante = new Fabricante;
+$fabricante->setNome("Renault");
+$fabricante->setSigla("RN");
+array_push($fabricantes,$fabricante);
+
+function Vincular($sigla, $fabricantes){
+
+    if ($sigla == "VW" or "GM" or "F" or "RN"){
+         foreach ($fabricantes as $fabricante) {
+            if($sigla == $fabricante->getSigla()) {
+                return $fabricante;
+        } 
+     }
+    } else {
+        return false;
+    }
+
+    
+
+}
+
+do {
+
+    echo "\nEscolha uma das opções abaixo: \n";
+    echo "1- Cadastrar carro.\n";
+    echo "2- Excluir carro.\n";
+    echo "3- Listar carros.\n";
+    echo "0- Sair.\n";
+    $opcao = readline("");
+
+    switch ($opcao) {
+        case 1:
+            $carro = new Carro;
+            $carro->setModelo(readline("Informe o modelo do carro: "));
+            $carro->setAnoFabricacao(readline("Informe o ano de fabricação do carro: "));
+            echo "Informe a sigla de um dos fabricantes abaixo: \n";
+            echo "VW - Volkswagem.\nGM - Chevrolet.\nF - Fiat.\nRN - Renault. \n";
+            $sigla = readline("");
+            $carro->setFabricante(Vincular($sigla, $fabricantes));
+            echo "Carro cadastrado com sucesso.\n";
+            break;
+
+        case 2:
+            $quantItens = count($fabricantes);
+            $indiceRemocao = readline("Informe o índice do carro que deseja remover: ");
+
+            if ($indiceRemocao < $quantItens) {
+                array_splice($fabricantes, $indiceRemocao, 1);
+                echo "Remoção concluída com sucesso! \n";
+            } else {
+                echo "Indice não disponível.\n";
+            }
+            break;
+        
+        default:
+            
+            break;
+    }
+
+} while ($opcao != 0);
