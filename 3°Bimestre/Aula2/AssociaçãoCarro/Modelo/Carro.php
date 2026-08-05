09e70772e1baa7cf27062903b13cac2e3e214a2a
Isabella Heinz <isabella.heinz12@gmail.com>
2026-08-05 15:47:21 -0300

    Add files via upload

---
+++ 3°Bimestre/Aula2/AssociaçãoCarro/Modelo/Carro.php
@@ -0,0 +1,53 @@
+<?php
+
+require_once("Fabricante.php");
+
+class Carro {
+    private string $modelo;
+    private int $anoFabricacao;
+    private Fabricante $fabricante;
+
+    public function __toString()
+    {
+        $dados = "Modelo: " . $this->modelo . " | Ano de fabricação: " . $this->anoFabricacao . " | " . $this->fabricante;
+        return $dados;
+    }
+
+    
+
+    public function getModelo(): string
+    {
+        return $this->modelo;
+    }
+
+    public function setModelo(string $modelo): self
+    {
+        $this->modelo = $modelo;
+
+        return $this;
+    }
+
+    public function getAnoFabricacao(): int
+    {
+        return $this->anoFabricacao;
+    }
+
+    public function setAnoFabricacao(int $anoFabricacao): self
+    {
+        $this->anoFabricacao = $anoFabricacao;
+
+        return $this;
+    }
+
+    public function getFabricante(): Fabricante
+    {
+        return $this->fabricante;
+    }
+
+    public function setFabricante(Fabricante $fabricante): self
+    {
+        $this->fabricante = $fabricante;
+
+        return $this;
+    }
+}
---
+++ 3°Bimestre/Aula2/AssociaçãoCarro/Modelo/Fabricante.php
@@ -0,0 +1,38 @@
+<?php
+
+class Fabricante {
+    private string $nome;
+    private string $sigla;
+
+    public function __toString()
+    {
+        $dados = "Fabricante: " . $this->nome . " | Sigla: " . $this->sigla;
+        return $dados;
+    }
+
+    
+
+    public function getNome(): string
+    {
+        return $this->nome;
+    }
+
+    public function setNome(string $nome): self
+    {
+        $this->nome = $nome;
+
+        return $this;
+    }
+
+    public function getSigla(): string
+    {
+        return $this->sigla;
+    }
+
+    public function setSigla(string $sigla): self
+    {
+        $this->sigla = $sigla;
+
+        return $this;
+    }
+}
